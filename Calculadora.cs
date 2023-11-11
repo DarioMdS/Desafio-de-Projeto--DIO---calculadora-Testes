185150b4bd7a6af7b2e1ca4d54e6bfb4122b86e6
DarioMdS <143542762+DarioMdS@users.noreply.github.com>
2023-11-11 10:26:46 -0300

    Add files via upload
    Desafio concluído com sucesso!

---
+++ Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_de_Projeto_DIO_calculadora_Testes;
+{
+    public class Calculadora
+    {
+        private List<String> listaHistorico;
+        public Calculadora()
+        {
+            listaHistorico = new List<String>(); 
+        }
+        
+        public int somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            listaHistorico.Insert(0, "Resultado " + resultado);
+            return resultado;
+        }
+        public int subtrair(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            listaHistorico.Insert(0, "Resultado " + resultado);
+            return resultado;
+        }
+        public int multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            listaHistorico.Insert(0, "Resultado " + resultado);
+            return resultado;
+        }
+        public int dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            listaHistorico.Insert(0, "Resultado " + resultado);
+            return resultado;
+        }
+
+        public List<String> historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
+        }
+    }
+}
